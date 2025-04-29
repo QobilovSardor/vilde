30204d443966136caf97efc8cc30caa78e675211
SardorQobilov <note47297@gmail.com>
2025-04-29 15:30:08 +0500

    game slide finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // features__swiper
-const swiper = new Swiper('.features__swiper', {
+const featuresSwiper = new Swiper('.features__swiper', {
   spaceBetween: 35,
   slidesPerView: 5,
   autoplay: {
@@ -9,4 +9,15 @@ const swiper = new Swiper('.features__swiper', {
   loop: true,
   centeredSlides: true,
   roundLengths: true,
+});
+
+// game__slider
+const gameSlider = new Swiper('.game__slider', {
+  loop: false,
+  slidesPerView: 2,
+  centeredSlides: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
 });
